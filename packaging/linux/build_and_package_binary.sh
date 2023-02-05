a2fd3ee0dc489c12a0288df0d169fcfd71829865
Jeremy Stribling <strib@alum.mit.edu>
2023-02-05 11:41:41 -0800

    packaging: build "release" versions

--- packaging/linux/build_and_package_binary.sh
+++ packaging/linux/build_and_package_binary.sh
@@ -3,7 +3,8 @@
 set -euox pipefail
 
 here="$(dirname "${BASH_SOURCE[0]}")"
-build_root="${1:-/tmp/keybase_build_$(date +%Y_%m_%d_%H%M%S)}"
+mode="${1:-dev}"
+build_root="${2:-/tmp/keybase_build_$(date +%Y_%m_%d_%H%M%S)}"
 
-$here/build_binary.sh $build_root
+$here/build_binary.sh $mode $build_root
 $here/deb/package_binary.sh $build_root
--- packaging/version.sh
+++ packaging/version.sh
@@ -3,14 +3,17 @@
 set -e -u -o pipefail
 
 here="$(dirname "$BASH_SOURCE")"
+mode="${1:-dev}"
 
 version_file="$(dirname "$BASH_SOURCE")/../version.go"
 version="$(cat "$version_file" | grep 'Version =' | grep -oE '[0-9]+(.[0-9]+)+')"
 build=""
 
-current_date="$(date -u +%Y%m%d%H%M%S)" # UTC
-commit_short="$(git -C "$here" log -1 --pretty=format:%h || \
-    echo -n ${SOURCE_COMMIT:0:10})"
-build="-$current_date+$commit_short"
+if [ ! "$mode" = "release" ] ; then
+    current_date="$(date -u +%Y%m%d%H%M%S)" # UTC
+    commit_short="$(git -C "$here" log -1 --pretty=format:%h || \
+        echo -n ${SOURCE_COMMIT:0:10})"
+    build="-$current_date+$commit_short"
+fi
 
 echo "$version$build"
